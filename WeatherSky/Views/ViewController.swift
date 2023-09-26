a866e2d784f49516d141efbba516bebbe7194dcf
Alfin Indrawan <alfinindrawan54@gmail.com>
2023-09-26 16:51:13 +0700

    init project

---
+++ WeatherSky/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherSky
+//
+//  Created by Alfin on 26/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .blue
+  }
+
+
+}
+
