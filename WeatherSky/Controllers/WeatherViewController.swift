ed742141302496a9727004d2cd421d86b9387381
Alfin Indrawan <alfinindrawan54@gmail.com>
2023-12-14 19:39:26 +0700

    add purchase code (final)

--- WeatherSky/Controllers/WeatherViewController.swift
+++ WeatherSky/Controllers/WeatherViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import OSLog
 import RevenueCat
 import RevenueCatUI
+import WeatherKit
 
 class WeatherViewController: UIViewController {
   
@@ -20,15 +21,14 @@ class WeatherViewController: UIViewController {
     view.backgroundColor = .systemBackground
     setupView()
     getLocation()
-    
-    navigationItem.rightBarButtonItem = UIBarButtonItem(image: UIImage(systemName: "crown"), style: .done, target: self, action: #selector(didTapUpgrade))
+ 
   }
   
   @objc
   private func didTapUpgrade() {
     let vc = PaywallViewController()
     vc.delegate = self
-//    vc.modalPresentationStyle = .fullScreen
+    //    vc.modalPresentationStyle = .fullScreen
     present(vc, animated: true)
   }
   
@@ -39,13 +39,36 @@ class WeatherViewController: UIViewController {
       WeatherManager.shared.getWeather(for: location) { [weak self] in
         DispatchQueue.main.async {
           guard let currentWeather = WeatherManager.shared.currentWeather else { return }
-          self?.primaryView.configure(with: [
-            .current(viewModel: .init(model: currentWeather)),
-            .hourly(viewModel: WeatherManager.shared.hourlyWeather.compactMap({ .init(model: $0)
-            })),
-            .daily(viewModel: WeatherManager.shared.dailyWeather.compactMap({ .init(model: $0)
-            }))
-          ])
+          self?.createViewModels(currentWeather: currentWeather)
+        }
+      }
+    }
+  }
+  
+  private func createViewModels(currentWeather: CurrentWeather) {
+    var viewModels: [WeatherViewModel] = [
+      .current(viewModel: .init(model: currentWeather)),
+      .hourly(viewModel: WeatherManager.shared.hourlyWeather.compactMap({ .init(model: $0)
+      })),
+    ]
+    primaryView.configure(with: viewModels)
+    IAPManager.shared.isSubscribed { subscribed in
+      if subscribed {
+        viewModels.append(
+          .daily(viewModel: WeatherManager.shared.dailyWeather.compactMap({ .init(model: $0)
+          }))
+        )
+        DispatchQueue.main.async {
+          self.primaryView.configure(with: viewModels)
+          self.navigationItem.rightBarButtonItem = nil
+        }
+      } else {
+        DispatchQueue.main.async {
+          self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(systemName: "crown"),
+            style: .done,
+            target: self,
+            action: #selector(self.didTapUpgrade))
         }
       }
     }
@@ -63,10 +86,12 @@ class WeatherViewController: UIViewController {
 
 extension WeatherViewController : PaywallViewControllerDelegate {
   func paywallViewController(_ controller: PaywallViewController, didFinishPurchasingWith customerInfo: CustomerInfo) {
-    print("Purchased \(customerInfo)")
+    controller.dismiss(animated: true)
+    guard let currentWeather = WeatherManager.shared.currentWeather else { return }
+    createViewModels(currentWeather: currentWeather)
   }
   
   func paywallViewController(_ controller: PaywallViewController, didFinishRestoringWith customerInfo: CustomerInfo) {
-    print("Purchased \(customerInfo)")
+    controller.dismiss(animated: true)
   }
 }
--- WeatherSky/Managers/IAPManager.swift
+++ WeatherSky/Managers/IAPManager.swift
@@ -1,9 +1,18 @@
 import Foundation
+import RevenueCat
 
 final class IAPManager {
   static let shared = IAPManager()
   
   private init() {
     
+  }
+  func isSubscribed(completion: @escaping (Bool) -> Void) {
+    Purchases.shared.getCustomerInfo { info, error in
+      guard let subscriptions = info?.activeSubscriptions else { return }
+     print("\n\(subscriptions)")
+      completion(!subscriptions.isEmpty)
+      
+    }
   }
 }
